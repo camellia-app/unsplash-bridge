076ab3bd1885f339540fcffcdeed507c42018aa9
Petr Flaks <petr@flaks.dev>
2023-01-01 17:17:38 +0300

    build: output as module

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,21 @@ const path = require('path');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src', 'worker.ts'),
-  target: 'webworker',
   output: {
     filename: 'worker.js',
     path: path.resolve(__dirname, 'dist'),
+    module: true,
+    library: {
+      type: 'module',
+    },
   },
   devtool: 'source-map',
   mode: 'production',
   resolve: {
     extensions: ['.ts', '.js'],
+  },
+  experiments: {
+    outputModule: true,
   },
   module: {
     rules: [
